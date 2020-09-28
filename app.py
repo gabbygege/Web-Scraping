cf26044495769834761aded9ab40e87750cd640e
GEGE <gabbyfebriann@gmail.com>
2020-09-28 22:15:24 +0700

    create new file and code

---
+++ app.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+
+url = 'https://jadwalsholat.pkpu.or.id/?id=14'
+content = requests.get(url)
+
+response = bs4.BeautifulSoup(content.text, "html.parser")
+data = response.find_all('tr', 'table_highlight')
+print(data[0])
+
+sholat = {}
+i = 0
+for d in data:
+    if i == 1:
+        sholat['subuh'] = d.get_text()
+    elif i == 2:
+        sholat['zuhur'] = d.get_text()
+    elif i == 3:
+        sholat['ashar'] = d.get_text()
+    elif i == 4:
+        sholat['maghrib'] = d.get_text()
+    elif i == 5:
+        sholat['isya'] = d.get_text()
+    i += 1
+
+print(sholat)
+
+print(sholat['isya'])
